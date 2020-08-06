04e7654a4239378eefdab0a40b2f678454e1c72e
leofernandez97 <leonardofernandez2697@gmail.com>
2020-08-06 18:19:29 -0300

    Agrego products

--- js/products.js
+++ js/products.js
@@ -1,6 +1,48 @@
+var productsArray = [];
+
+function showProductsList(array){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let category = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + category.imgSrc + `" alt="` + category.description + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ category.name +`</h4>
+                        <small class="text-muted">` + category.soldCount + ` artículos</small>
+                        
+                    </div>
+                    <p> ` + category.description +` </p>
+                    <h5>` + category.currency + ` ` + category.cost + ` </h5>
+
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+    }
+}
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+// copie el codigo de categories y agregue PRODUCTS_URL
 document.addEventListener("DOMContentLoaded", function (e) {
-
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            productsArray = resultObj.data;
+            //Muestro las categorías ordenadas
+            showProductsList(productsArray);
+        }
+    });    
+       
 });
