516421f9132f93efd35fe24536e37b2d16454dd0
leofernandez97 <leonardofernandez2697@gmail.com>
2020-08-08 07:21:55 -0300

    Agrego login con validacion de usu y pass vacios

--- js/login.js
+++ js/login.js
@@ -1,3 +1,28 @@
+
+
+// Funcion que valida campos escritos.
+var status = null;
+function validate(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var expresion = /\w+@\w+\.+[a-z]/;
+
+    if (username === "" || password === ""){
+        alert("Debe ingresar usuario y contraseña");
+        return false;
+    }
+    else if (!expresion.test(username)){
+        alert("Correo inválido");
+        return false;
+    }
+    else{
+        
+        window.location.replace("index.html");
+        alert("Disfrute su estadia en la pagina");
+    }
+}
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
