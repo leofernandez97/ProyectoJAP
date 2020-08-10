d8bb25041a3502fd4433fa32659d4bc48d95d3c3
leofernandez97 <leonardofernandez2697@gmail.com>
2020-08-10 03:29:39 -0300

    Redireccion a login y validacion

--- js/login.js
+++ js/login.js
@@ -1,31 +1,24 @@
+// Funcion que valida logueo.
+function validate() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  var expresion = /\w+@\w+\.+[a-z]/;
 
-
-// Funcion que valida campos escritos.
-var status = null;
-function validate(){
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    var expresion = /\w+@\w+\.+[a-z]/;
-
-    if (username === "" || password === ""){
-        alert("Debe ingresar usuario y contraseña");
-        return false;
-    }
-    else if (!expresion.test(username)){
-        alert("Correo inválido");
-        return false;
-    }
-    else{
-        
-        window.location.replace("index.html");
-        alert("Disfrute su estadia en la pagina");
-    }
+  if (username === "" || password === "") {
+    alert("Debe ingresar usuario y contraseña");
+    return false;
+  } else if (!expresion.test(username)) {
+    alert("Correo inválido");
+    return false;
+  } else {
+    logueado = "true";
+    sessionStorage.setItem("estaLogueado", logueado);
+    window.location.replace("./index.html");
+    alert("Disfrute su estadia en la pagina");
+  }
 }
-
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-
-});
+document.addEventListener("DOMContentLoaded", function (e) {});
---
+++ js/redireccion.js
@@ -0,0 +1,21 @@
+let logueado = sessionStorage.getItem("estaLogueado");
+function exist(){
+  if (typeof(logueado) == "object") {
+    logueado = false; 
+    sessionStorage.setItem("estaLogueado", logueado);
+  }
+}
+
+function redireccion(){
+  if (logueado == "false") {
+    window.location.replace("./login.html");
+  }else if (logueado != "true"){
+    alert('Debe iniciar sesión')
+    window.location.replace("./login.html");
+    
+  }
+}
+
+
+exist();
+redireccion();
