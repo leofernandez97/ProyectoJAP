18015540bbb333cc3d478f82e6301cad32bd3675
leofernandez97 <leonardofernandez2697@gmail.com>
2020-08-31 18:47:44 -0300

    Comments

--- js/product-info.js
+++ js/product-info.js
@@ -1,4 +1,5 @@
 var product = {};
+let comment = {};
 
 function showImagesGallery(array){
 
@@ -8,8 +9,8 @@ function showImagesGallery(array){
         let imageSrc = array[i];
 
         htmlContentToAppend += `
-        <div class="col-lg-3 col-md-4 col-6">
-            <div class="d-block mb-4 h-100">
+        <div class="col-lg-3 col-md-4 col-6 w-100">
+            <div class="d-block mb-4 w-100">
                 <img class="img-fluid img-thumbnail" src="` + imageSrc + `" alt="">
             </div>
         </div>
@@ -28,8 +29,8 @@ function showRelatedProducts(array){
 
         htmlContentToAppend += `
         <div class="col-lg-3 col-md-4 col-6">
-            <div class="d-block mb-4 h-100">
-                <img class="img-fluid img-thumbnail" src="img/car` + relatedProducts + `.jpg" alt="">
+            <div class="d-block mb-4 w-100">
+                <a href="product-info.html"> <img class="img-fluid img-thumbnail" src="img/car` + relatedProducts + `.jpg" alt=""></a>
             </div>
         </div>
         `
@@ -37,6 +38,31 @@ function showRelatedProducts(array){
         document.getElementById("productRelatedProducts").innerHTML = htmlContentToAppend;
     }
 }
+function showComments(array){
+        let htmlContentToAppend = "";
+        for(let i = 0; i < array.length; i++){
+            let comment = array[i];
+
+            htmlContentToAppend += `
+            <div class="card mb-2">
+                <div class="card-header">
+                `+comment.user+comment.score+`
+                </div>
+                <div class="card-body ">
+                    <blockquote class="blockquote mb-0">
+                    <p>`+comment.description+`</p>
+                    <div class="blockquote-footer m-0">`+comment.dateTime+`</div>
+                    </blockquote>
+                </div>
+            </div>
+            `
+    
+            document.getElementById("comentarios-prod").innerHTML = htmlContentToAppend;
+        }
+    }
+
+    
+
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
@@ -64,11 +90,15 @@ document.addEventListener("DOMContentLoaded", function(e){
             showRelatedProducts(product.relatedProducts);
         }
     });
-});
 
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
+        if(resultObj.status === "ok")
+        {
+            comment = resultObj.data;
 
-});
+            
+            showComments(comment);
+        }
+    
+    });
+    });
